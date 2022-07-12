6829d4a2f229d499efb10ef847d1a100f6dbaeab
dominic <oss7140@naver.com>
2022-07-12 16:45:16 +0900

    22.06 secondProject

---
+++ src/test/java/kr/human/mybatis/MybatisAppTest.java
@@ -0,0 +1,28 @@
+package kr.human.mybatis;
+
+import static org.junit.Assert.assertNotNull;
+
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class MybatisAppTest {
+	
+	static SqlSessionFactory sqlSessionFactory;
+	
+	@Before
+	public void beforeClass() {
+		sqlSessionFactory = MybatisApp.getSqlSessionFactory();
+	}
+	
+	@Test
+	public void getSession() {
+		assertNotNull(MybatisApp.getSqlSessionFactory());
+	}
+
+
+	
+}
